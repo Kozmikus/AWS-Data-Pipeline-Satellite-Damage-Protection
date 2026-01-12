62e724d593d92dfa94f423ba442c19dcb8689b01
Kozmikus <margiticslaci2003@gmail.com>
2026-01-12 13:15:53 +0100

    Changed script of Lambda API handler

--- code/space_weather_lambda_function.py
+++ code/space_weather_lambda_function.py
@@ -1,9 +1,13 @@
 import urllib.request
 import json
 import boto3
+from datetime import datetime
 
-# config:
-API_KEY = "ZoIR3ASieEkYkcd02Cj7Jx2TMxqwJxh6hFekHvUk"  
+# Config:
+API_KEY = "ZoIR3ASieEkYkcd02Cj7Jx2TMxqwJxh6hFekHvUk" # NOT SAFE TO HARD CODE IN PRODUCTION! 
+# irl this should be stored in AWS Secrets Manager or as an encrypted environment variable
+# for the sake of simplicity in this example I will hardcode it here
+
 url = f"https://api.nasa.gov/DONKI/GST?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key={API_KEY}"
 
 DESTINATION_BUCKET = "satellite-protector-bucket-yzu8r3"
@@ -12,26 +16,39 @@ DESTINATION_FOLDER = "raw_nasa_donki_weather/"
 def lambda_handler(event, context):
     
     try:
-        # Fetch the space weather data
+        # Fetching space weather data from NASA DONKI API
         with urllib.request.urlopen(url) as response:
-            if response is not None:
-                data = json.loads(response.read().decode())
-            else:
-                raise ValueError("No response received from the API")
+            if response.status != 200:
+                raise Exception(f"API fetching failed: {response.status} - {response.reason}")
+            data = json.loads(response.data.decode('utf-8')) # decodes bytes to string before loading in JSON format
 
-        latest_data = data[0]
-        magnetic_field = latest_data.get('bt', 'N/A')
+        if not data:
+            raise Exception("No data retrieved from NASA API. Terminating execution.") # handling empty data case and raising an 
+                                                                # exception for Lambda to capture and abort execution
         
-        print(f"Latest Magnetic Field Data: {magnetic_field}")
-        
+        print(f"Successfully retrieved {len(data)} records from NASA API.")
+
+        # Preparing the upload file to S3
+        s3_client = boto3.client('s3')
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = f"gst_data_{timestamp}.json"
+        s3_path = f"{DESTINATION_FOLDER}{file_name}"
+
+        # Uploading data to S3
+        s3_client.put_object(
+            Bucket=DESTINATION_BUCKET,
+            Key=s3_path,
+            Body=json.dumps(data), # json.dumps to convert Python object back to JSON string
+            ContentType='application/json'
+        ) # uploading JSON data as a string and setting content type
+
+        print(f"Data has been successfully uploaded to s3://{DESTINATION_BUCKET}/{s3_path}")
+
         return {
             'statusCode': 200,
-            'body': json.dumps('Space weather data processed successfully!')
+            'body': json.dumps(f'Space weather data processed and uploaded successfully with {file_name} to s3://{DESTINATION_BUCKET}/{s3_path}.')
         }
     
     except Exception as e:
-        print(f"Error fetching or processing space weather data, specifically: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Error processing space weather data.')
-        }
+        print(f"FATAL ERROR: {str(e)}")
+        raise e  # raise exception for AWS Lambda to capture the failure and abort execution
